ad32bad45e0416462d8c1c0d0769aca1caaf2a5c
Ryan <rycr3278@colorado.edu>
2023-06-13 19:09:39 +0000

    Completed pa2-rcrosss17

--- code/LinkedList.cpp
+++ code/LinkedList.cpp
@@ -2,8 +2,8 @@
 
 // constructor, initialize class variables and pointers here if need.
 LinkedList::LinkedList(){
-  // Your code here
 }
+
 
 //deconstructor,
 LinkedList::~LinkedList(){
@@ -11,46 +11,139 @@ LinkedList::~LinkedList(){
 
 
 shared_ptr<node> LinkedList::InitNode(int data){
+  //allocate memory for new node
   shared_ptr<node> ret(new node);
-  // Your code here
+  //fill in struct info
+  ret->data = data;
+  ret->next = nullptr;
   return ret;
 }
 
 string LinkedList::Report(){
-  string ret;
-  // Your code here
+  string ret = "";
+
+  //create current node pointer and start at head
+  shared_ptr<node> curr = top_ptr_;
+  //traverse through entire list to the tail
+  while(curr != nullptr) {
+    //record data from each node into string
+    ret = ret + to_string(curr->data) + " ";
+    curr = curr->next;
+  }
+
   return ret;
 }
 
 void LinkedList::AppendData(int data){
-  // Your code here
+  //create new node
+  shared_ptr<node> new_node = InitNode(data);
+  //call Append function
+  Append(new_node); 
 }
 
 void LinkedList::Append(shared_ptr<node> new_node){
-  // Your code here
+  //check if list is empty
+  if (top_ptr_ == nullptr){
+    top_ptr_ = new_node;
+  }
+  //traverse through list to find the end
+  else {
+    //create current node pointer and start at head
+    shared_ptr<node> curr = top_ptr_;
+    while(curr->next !=nullptr) {
+      curr = curr->next;
+    }
+    //add new_node to linked list at the end
+    curr->next = new_node;
+
+  }
 }
 
 void LinkedList::InsertData(int offset, int data){
-  // Your code here
+  //create new node
+  shared_ptr<node> new_node = InitNode(data);
+  //check that offset is valid
+  if (offset < 0) {
+    return;
+  }
+  //check if offset is zero. If yes, make new_node head of list
+  else if (offset == 0) {
+    new_node->next = top_ptr_;
+    top_ptr_ = new_node;
+    return;
+  }
+  //traverse list to find offset position
+  else {
+  shared_ptr<node> curr = top_ptr_;
+    //stop traversal if end of list reached before offset position reached
+    int pos_count = 0;
+    while(curr != nullptr && pos_count < offset - 1){
+        curr = curr->next;
+        pos_count++;
+      }
+    
+    //add new_node at offset position
+    new_node->next = curr->next;
+    curr->next = new_node;
+    
+  }
 }
 
 void LinkedList::Insert(int offset, shared_ptr<node> new_node){
-  // Your code here
+  InsertData(offset, new_node->data);
 }
 
 void LinkedList::Remove(int offset){
-  // Your code here
+   //check that offset is valid
+  if (offset < 0) {
+    return;
+  }
+  //check if offset is zero. 
+  else if (offset == 0) {
+    if(top_ptr_->next != nullptr){
+      //remove top pointer by setting next pointer as new top
+      SetTop(top_ptr_->next);
+    }
+    return;
+  }
+  //traverse list to find offset position
+  else {
+  shared_ptr<node> curr = top_ptr_;
+    //stop traversal if end of list reached before offset position reached
+    int pos_count = 0;
+    while(pos_count < offset - 1){
+        curr = curr->next;
+        pos_count++;
+      }
+    
+    if (curr != nullptr){
+      shared_ptr<node> temp = curr->next;
+      curr->next = curr->next->next;
+      
+    }
+  }
 }
 
 int LinkedList::Size(){
-  int ret;
-  // Your code here
+  int ret = 0;
+  shared_ptr<node> curr = top_ptr_;
+    while(curr != nullptr) {
+      //count each node traversed
+      curr = curr->next;
+      ret++;
+    }
   return ret;
 }
 
 bool LinkedList::Contains(int data){
   bool ret;
-  // Your code here
+  shared_ptr<node> curr = top_ptr_;
+    while(curr != nullptr) {
+      if(curr->data == data) {
+        ret = true;
+      }
+      curr = curr->next;
+    }
   return ret;
 }
 
